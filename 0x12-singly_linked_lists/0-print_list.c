0479ccd411f2482fecd7a6a88509154a20447971
Victor <vicmwiti@gmail.com>
2022-11-22 15:06:27 -0800

    correction of task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_list - prints all the elements of a linked list
  * @h: head of list
  *
  * Return: the number of nodes
