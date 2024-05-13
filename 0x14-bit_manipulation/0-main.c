c78ba88094034f4d885c2eafd6a1e549055cbdc4
vick42 <vicmwiti@gmail.com>
2024-05-13 06:01:33 -0700

    correction

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	unsigned int n;
 
-	n = binary_to_unit("1");
+	n = binary_to_uint("1");
 	printf("%u\n", n);
-	n = binary_to_unit("101");
+	n = binary_to_uint("101");
 	printf("%u\n", n);
-	n = binary_to_unit("1e01");
+	n = binary_to_uint("1e01");
 	printf("%u\n", n);
-	n = binary_to_unit("1100010");
+	n = binary_to_uintt("1100010");
 	printf("%u\n", n);
-	n = binary_to_unit("0000000000000000000110010010");
+	n = binary_to_uint("0000000000000000000110010010");
 	printf("%u\n", n);
 	return (0);
 }
