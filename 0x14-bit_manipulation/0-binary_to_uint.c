87520e972841bfc2113a60a09b6c1cafd28bc3e7
Vick42 <vicmwiti@gmail.com>
2024-06-12 11:41:29 +0000

    reviewed binary error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int base = 1;
+	unsigned int temp = 0, result = 0;
+	int len = 0;
 
 	if (!b)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	while (b[len])
+		len++;
+	while (len > 0)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		temp = (b[len - 1] == '1') ? 1 : 0;
+		result += (temp * base);
+		base *= 2;
+		len--;
 	}
-
-	return (ui);
+	return (result);
 }
